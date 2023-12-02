fc951da192a12a39e710a29b1a9ba180641cadf4
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2023-12-02 16:23:50 +0700

    aaaa

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Action Github</title>
+</head>
+<body>
+    <h3>Welcome to PHP</h3>
+</body>
+</html>
